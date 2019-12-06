fcf3ffc676a5b4f6ce64739e083fb73e68e828f1
seung young oh <46865281+SeungYoungOh@users.noreply.github.com>
2019-12-06 21:36:07 +0900

    BST 수정

--- Library-Mangement-System/BinarySearchTree.h
+++ Library-Mangement-System/BinarySearchTree.h
@@ -1,9 +1,9 @@
 #pragma once
-
-#ifndef mBINARYSEARCHmTREEmH
-#define mBINARYSEARCHmTREEmH
 #include <iostream>
+
+#include <functional>
 using namespace std;
+enum RelationType { LESS, GREATER, EQUAL };
 
 
 template <typename T>
@@ -20,12 +20,30 @@ class BinarySearchTree
 {
 private:
 	TreeNode<T>* mRoot;
+	std::function<RelationType(const T&, const T&)> mCompFunc;
+
 
 public:
 	BinarySearchTree()
 	{
 		mRoot = NULL;
+		mCompFunc = [](const T& a, const T& b)->RelationType {
+			if (a == b)
+				return RelationType::EQUAL;
+			else if (a > b)
+				return RelationType::GREATER;
+			else
+				return RelationType::LESS;
+		};
 	}
+
+	BinarySearchTree(std::function<RelationType(const T&, const T&)> comp)
+		: mCompFunc(comp)
+	{
+		mRoot = NULL;
+	}
+
+
 	~BinarySearchTree()
 	{
 		Destroy(mRoot);
@@ -70,12 +88,12 @@ public:
 		mRoot = NULL;
 	}
 
-	int CountNodes(TreeNode<T>* tree)
+	int CountNodes(TreeNode<T>* treePtr)
 	{
-		if (tree == NULL)
+		if (treePtr == NULL)
 			return 0;
 		else
-			return CountNodes(tree->left) + CountNodes(tree->right) + 1;
+			return CountNodes(treePtr->left) + CountNodes(treePtr->right) + 1;
 	}
 
 	/**
@@ -90,12 +108,11 @@ public:
 			treePtr->mLeft = NULL;
 			treePtr->mData = item;
 		}
-
-		else if (item < treePtr->mData)
+		else if (mCompFunc(treePtr->mData, item) == RelationType::GREATER)
 		{
 			InternalInsert(treePtr->mLeft, item);
 		}
-		else if (item > treePtr->mData)
+		else if (mCompFunc(treePtr->mData, item) == RelationType::LESS)
 		{
 			InternalInsert(treePtr->mRight, item);
 		}
@@ -105,70 +122,72 @@ public:
 	*/
 	void InternalDelete(TreeNode<T>*& treePtr, T item)
 	{
-		if (item < treePtr->mData)
+		if (mCompFunc(treePtr->mData, item) == RelationType::GREATER)
 			InternalDelete(treePtr->mLeft, item);
-		else if (item > treePtr->mData)
+		else if (mCompFunc(treePtr->mData, item) == RelationType::LESS)
 			InternalDelete(treePtr->mRight, item);
 		else
 			DeleteNode(treePtr); // node found.
 	}
 
-	void DeleteNode(TreeNode<T>*& tree)
+	void DeleteNode(TreeNode<T>*& treePtr)
 	{
 		T tempItem;
 		TreeNode<T>* tempPtr;
-		tempPtr = tree;
-		if (tree->mLeft == NULL)
+		tempPtr = treePtr;
+		if (treePtr->mLeft == NULL)
 		{
-			tree = tree->mRight;
+			treePtr = treePtr->mRight;
 			delete tempPtr;
 		}
-		else if (tree->mRight == NULL)
+		else if (treePtr->mRight == NULL)
 		{
-			tree = tree->mLeft;
+			treePtr = treePtr->mLeft;
 			delete tempPtr;
 		}
 		else
 		{
 			//지우려는 노드의 자식노드에서 최댓값 찾은 후 그걸로 대체한 다음 본래 최댓값 지움
-			GetPredecessor(tree->mLeft, tempItem);
-			tree->mData = tempItem;
-			InternalDelete(tree->mLeft, tempItem);
+			GetPredecessor(treePtr->mLeft, tempItem);
+			treePtr->mData = tempItem;
+			InternalDelete(treePtr->mLeft, tempItem);
 		}
 	}
 
-	void GetPredecessor(TreeNode<T>* tree, T& data)
+	void GetPredecessor(TreeNode<T>* treePtr, T& data)
 	{
-		while (tree->mRight != NULL)
-			tree = tree->mRight;
-		data = tree->mData;
+		while (treePtr->mRight != NULL)
+			treePtr = treePtr->mRight;
+		data = treePtr->mData;
 	}
 
-	bool InternalRetrieve(TreeNode<T>* root, T& item)
+	bool InternalRetrieve(TreeNode<T>* treePtr, T& item)
 	{
-		if (root == NULL)
+		if (treePtr == NULL)
 		{
 			return false;
 		}
-		else if (item < root->mData)
+		else if (mCompFunc(treePtr->mData, item) == RelationType::GREATER)
 		{
-			InternalRetrieve(root->mLeft, item);
+			InternalRetrieve(treePtr->mLeft, item);
 		}
-		else if (item > root->mData)
-			InternalRetrieve(root->mRight, item);
-		else
+		else if (mCompFunc(treePtr->mData, item) == RelationType::LESS)
+			InternalRetrieve(treePtr->mRight, item);
+		else if (mCompFunc(treePtr->mData, item) == RelationType::EQUAL)
 		{
-			item = root->mData;
+			item = treePtr->mData;
 			return true;
 		}
+		return false;
 	}
 
+	//수정 해야함
 	void InternalPrint(TreeNode<T>* treePtr) //, ofstream& outFile
 	{
 		if (treePtr != NULL)
 		{
 			InternalPrint(treePtr->mLeft);
-			cout << treePtr->mData.GetId() << " - ";
+			cout << treePtr->mData.GetAuthor() << " - ";
 			InternalPrint(treePtr->mRight);
 		}
 	}
@@ -207,10 +226,7 @@ public:
 		InternalPrint(mRoot);
 		cout << "\n";
 	}
+
 };
 
 
-
-
-
-#endif
